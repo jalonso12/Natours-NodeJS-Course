8848caf26fe667a7dbe2420b1dcf4091a63654f5
jalonso12 <jalonsoo514@ulacit.ed.cr>
2020-08-26 22:10:37 -0600

    Error Handling
    - AppError class made for organized error displaying
    
    - Operational error handler
    
    - Get, Post, Put, Delete, etc endpoints error handler function
    
    - Unnecessary middleware example commented

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 
 // ROUTE MODULES
+const AppError = require('./utils/appError.js');
+const errorHandler = require('./handlers/errorController.js');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -16,10 +19,10 @@ APP.use(express.json());
 APP.use(express.static(`${__dirname}/public`));
 
 // Local Middleware
-APP.use((req, res, next) => {
-    //console.log('Middleware hobbit in action');
-    next();
-});
+// APP.use((req, res, next) => {
+//     //console.log('Middleware hobbit in action');
+//     next();
+// });
 
 APP.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -29,6 +32,13 @@ APP.use((req, res, next) => {
 // MOUNT ROUTING
 APP.use('/api/v1/tours', tourRouter);
 APP.use('/api/v1/users', userRouter);
+
+// Unexisting endpoint handdling
+APP.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404)); // Param only applies for errors
+});
+
+APP.use(errorHandler);
 
 // Export app for server
 module.exports = APP;
