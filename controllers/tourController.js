a74fe5aaaa247959885e1f418097092564cc5197
jalonso12 <jalonsoo514@ulacit.ed.cr>
2020-08-26 23:12:55 -0600

    404 Error Handling
    - Added 404 id not found handler statement
    
    - Minor extra tabs fixed

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('../models/tourModel.js');
 const APIFeatures = require('./../utils/apiFeatures.js');
-const catchAsync = require('./../handlers/errorCatchHandler.js');
+const catchAsync = require('../handlers/errorCatchHandler.js');
+const AppError = require('../utils/appError.js');
 
 // Adds data to the API and JSON file.
 exports.createTour = catchAsync(async (req, res, next) => {
@@ -19,19 +20,19 @@ exports.createTour = catchAsync(async (req, res, next) => {
 // Creates the API with the data information plus some additions (status & results)
 exports.getAllTours = catchAsync(async (req, res, next) => {
     const features = new APIFeatures(Tour.find(), req.query).filter()
-                                                                .sort()
-                                                                .limitFields()
-                                                                .paginate();
-        const tours = await features.query;
+                                                            .sort()
+                                                            .limitFields()
+                                                            .paginate();
+    const tours = await features.query;
 
-        res
-            .status(200)
-            .json({
-                status: 'success',
-                data: {
-                    tours
-                }
-            });
+    res
+        .status(200)
+        .json({
+            status: 'success',
+            data: {
+                tours
+            }
+        });
 });
 
 exports.aliasTopTours = (req, res, next) => {
@@ -44,20 +45,20 @@ exports.aliasTopTours = (req, res, next) => {
 };
 
 exports.getSpecificTour = catchAsync(async (req, res, next) => {
-    console.log(req.query);
+    const id = req.params.id;
 
-        const id = req.params.id;
+    const tour = await Tour.findById(id);
 
-        const tour = await Tour.findById(id);
+    if(!tour) return next(new AppError('No tour found with that ID', 404));
 
-        res
-            .status(200)
-            .json({
-                status: 'success',
-                data: {
-                    tour
-                }
-            });
+    res
+        .status(200)
+        .json({
+            status: 'success',
+            data: {
+                tour
+            }
+        });
 });
 
 exports.updateTour = (async (req, res, next) => {
@@ -67,6 +68,8 @@ exports.updateTour = (async (req, res, next) => {
         new: true,
         runValidators: true
     });
+
+    if(!tour) return next(new AppError('No tour found with that ID', 404));
 
     res
         .status(200)
@@ -81,14 +84,16 @@ exports.updateTour = (async (req, res, next) => {
 exports.deleteTour = (async (req, res, next) => {
     const id = req.params.id;
 
-        await Tour.findByIdAndDelete(id);
+    const tour = await Tour.findByIdAndDelete(id);
 
-        res
-            .status(204)
-            .json({
-                status: 'success',
-                data: null
-            });
+    if(!tour) return next(new AppError('No tour found with that ID', 404));
+
+    res
+        .status(204)
+        .json({
+            status: 'success',
+            data: null
+        });
 });
 
 exports.getTourStats = (async (req, res, next) => {
