05ffc5e0047d8bd04c3d4bd127f6e8875478f8c2
jalonso12 <jalonsoo514@ulacit.ed.cr>
2020-08-30 17:08:24 -0600

    Dev - Prod Errors
    - Different error handling messages to dev and prod
    
    - Minor typing fixes

--- handlers/errorGlobalHandler.js
+++ handlers/errorGlobalHandler.js
@@ -1,11 +1,41 @@
-module.exports = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-
+const sendErrorDev = (err, res) => {
     res
         .status(err.statusCode)
         .json({
             status: err.status,
-            message: err.message
+            error: err,
+            message: err.message,
+            stack: err.stack
         });
+}
+
+const sendErrorProd = (err, req, res, next) => {
+    if(err.isOperational) {
+        res
+            .status(err.statusCode)
+            .json({
+                status: err.status,
+                message: err.message
+            });
+    } else {
+        console.error('ERROR ', err);
+
+        res 
+            .status(500)
+            .json({
+                status: 'error',
+                message: 'Something went very wrong!'
+            })
+    };
+}
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    if(process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    }else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(err, res);
+    };
 };
