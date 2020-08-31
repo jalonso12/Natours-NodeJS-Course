878bbc0212cf9841d9875b23dbbec9434dc3abd1
jalonso12 <jalonsoo514@ulacit.ed.cr>
2020-08-30 20:19:08 -0600

    Uncaught Exception Handler
    - Fixed unhandled rejection
    
    - Added uncaught exception

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+
+    console.log('Shutting down program...');
+    process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
@@ -12,17 +19,19 @@ mongoose.connect(DB, {
     useUnifiedTopology: true
 }).then(() => console.log('DB connection stablished!'));
 
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+
+    console.log('Shutting down program...');
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
 const APP = require('./app');
 
 // SERVER STARTER
 const port = process.env.PORT || 8000;
-APP.listen(port, 'localhost', () => {
+const server = APP.listen(port, 'localhost', () => {
     console.log(`<--- App running on ${process.env.NODE_ENV} (Port: ${port}) --->`);
-});
-
-process.on('unhandledRejection', err => {
-    console.log(err.name, err.message);
-
-    console.log('Shutting down program...')
-    process.exit(1);
 });
